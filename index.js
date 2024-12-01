50d0968c85d4b49123c6afb746a1c2fe4c71da74
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-12-01 13:04:49 +0600

    delete done

--- index.js
+++ index.js
@@ -86,9 +86,23 @@ async function run() {
 
 
         // Users Api
+
+        app.get("/users", async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
         app.post("/users", async(req, res)=>{
             console.log(req.body);
             const result = await userCollection.insertOne(req.body);
+            res.send(result);
+        })
+
+        app.delete("/users/:id", async(req, res)=>{
+            // const id = req.params.id;
+            // const query = {_id: new ObjectId(id)}
+            const query = {_id: new ObjectId(req.params.id)};
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
